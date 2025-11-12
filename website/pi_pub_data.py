20f8c1385c6afcab656839d37511f576aa669eff
mannaja <manman7852078520@gmail.com>
2025-11-13 00:02:07 +0700

    update web

--- website/pi_pub_data.py
+++ website/pi_pub_data.py
@@ -1,4 +1,4 @@
 import json, paho.mqtt.client as mqtt
-cli = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2); cli.connect("YOUR_SERVER_IP",1883)
+cli = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2); cli.connect("192.168.50.242",1883) # <-- ใส่ IP คอมที่รัน mosquitto
 cmd = {"cmd":"set_rate","hz":5}
 cli.publish("cmd/cam/cam_01", json.dumps(cmd), qos=1)
